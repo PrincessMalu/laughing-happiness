2e2b884a9e7fbbac335a0a22700955e61b29be68
PrincessMalu <64325013+PrincessMalu@users.noreply.github.com>
2020-05-21 19:14:40 -0700

    Add files via upload

--- APCSFinal - Poker/Player.java
+++ APCSFinal - Poker/Player.java
@@ -2,84 +2,113 @@
 import java.awt.Color;
 import java.util.*;
 
+
 public class Player
 {
     private static final Object RED = Color.RED;
+
     private static final Object BLUE = Color.BLUE;
+
     private static final Object GREEN = Color.GREEN;
+
     private static final Object BLACK = Color.BLACK;
+
     private String name;
+
     private int amount;
-    Chips [] playerChips;
+
+    Chips[] playerChips;
+
     public ArrayList<Card> hand = new ArrayList<Card>();
-    
-    public Player (String n, Chips [] play, Card c, Card d)
+
+
+    public Player(
+        String n,
+        Chips[] play,
+        Card c,
+        Card d,
+        Card card1,
+        Card card2,
+        Card card3 )
     {
         name = n;
         playerChips = play;
         hand.add( c );
         hand.add( d );
-        
-        for (Chips ch: play)
+        hand.add( card1 );
+        hand.add( card2 );
+        hand.add( card3 );
+
+        for ( Chips ch : play )
         {
-            if (ch.getColor().equals( RED ))
+            if ( ch.getColor().equals( RED ) )
             {
                 amount += 5;
             }
-            else if (ch.getColor().equals( BLUE ))
+            else if ( ch.getColor().equals( BLUE ) )
             {
                 amount += 10;
             }
-            else if (ch.getColor().equals( GREEN))
+            else if ( ch.getColor().equals( GREEN ) )
             {
                 amount += 25;
             }
-            else if (ch.getColor().equals( BLACK ))
+            else if ( ch.getColor().equals( BLACK ) )
             {
                 amount += 100;
             }
-            
-                
+
         }
     }
-    
+
+
     public String getName()
     {
         return name;
     }
-    
-    /*public Card addCard ()
+
+
+    public void setHand( Card c )
     {
-        
+        hand.add( c );
+
     }
-    */
-    public String addName (String s)
+
+
+    public String addName( String s )
     {
         name = s;
         return name;
     }
+
+
     public int getAmount()
     {
         return amount;
     }
+
+
     public void introduce()
     {
-        System.out.println ("Hello, my name is" + name);
+        System.out.println( "Hello, my name is" + name );
     }
-    
-    public void bet (int bet)
+
+
+    public void bet( int bet )
     {
-       amount =- bet;
+        amount = -bet;
     }
-    
-    public void earn (int money)
+
+
+    public void earn( int money )
     {
         amount += money;
     }
-    public void addMoney (int amount1)
+
+
+    public void addMoney( int amount1 )
     {
         amount += amount1;
     }
-    
 
 }
